da23606e63bf90419a55fb16aa0ee3c6afb1c32b
rusweb96 <rusweb96@gmail.com>
2023-09-28 16:57:12 +0600

    Урок 5, изменен модель рейтинг -> movie -> аргумент в место charfild поставлен CASCADE

--- movies/models.py
+++ movies/models.py
@@ -106,7 +106,7 @@ class Rating(models.Model):
     """ Рейтинг """
     ip = models.CharField('IP адрес', max_length=15)
     star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name='Звезда')
-    movie = models.ForeignKey(Movie, on_delete=models.CharField, verbose_name='Фильм')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='Фильм')
 
     def __str__(self):
         return f'{self.star} - {self.movie}'
