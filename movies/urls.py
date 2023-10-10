45fde032845e21d5a2c8335c430cbd763733c811
rusweb96 <rusweb96@gmail.com>
2023-10-10 19:37:15 +0600

    16 урок, Страница актера

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls.conf import path
-from .views import MoviesViews, MovieDetailsViews, AddReview
+from .views import MoviesViews, MovieDetailsViews, AddReview, ActorView
 
 urlpatterns = [
     path('', MoviesViews.as_view(), name='index'),
     path('<slug:slug>/', MovieDetailsViews.as_view(), name='movie_details'),
     path('review/<int:pk>/', AddReview.as_view(), name='add_review'),
+    path('actor/<str:slug>/', ActorView.as_view(), name='actor_detail'),
 ]
